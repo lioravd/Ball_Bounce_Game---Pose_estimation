7f938935846a23986dcce86ca1aa46f10dac9398
lioravd <113434611+lioravd@users.noreply.github.com>
2023-06-08 11:37:13 +0300

    Add files via upload

--- pose_estimation.py
+++ pose_estimation.py
@@ -1,60 +1,24 @@
-# Lint as: python3
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-r"""Example using PyCoral to estimate a single human pose with Edge TPU MoveNet.
-To run this code, you must attach an Edge TPU to the host and
-install the Edge TPU runtime (`libedgetpu.so`) and `tflite_runtime`. For
-device setup instructions, see coral.ai/docs/setup.
-For more details about MoveNet and its best practices, please see
-https://www.tensorflow.org/hub/tutorials/movenet
-Example usage:
-```
-bash examples/install_requirements.sh movenet_pose_estimation.py
-python3 examples/movenet_pose_estimation.py \
-  --model test_data/movenet_single_pose_lightning_ptq_edgetpu.tflite  \
-  --input test_data/squat.bmp
-```
-"""
-
 import argparse
-
 import numpy as np
 from PIL import Image
 from PIL import ImageDraw
 from pycoral.adapters import common
 from pycoral.utils.edgetpu import make_interpreter
 import numpy as np
+import cv2
 _NUM_KEYPOINTS = 17
-
-
 def det_pose(input):
     parser = argparse.ArgumentParser(
       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
       '-m', '--model', required=True, help='File path of .tflite file.')
     args = parser.parse_args()
-
-
-
     interpreter = make_interpreter(args.model)
     interpreter.allocate_tensors()
     img = Image.fromarray(inp)
     resized_img = img.resize(common.input_size(interpreter), Image.ANTIALIAS)
     common.set_input(interpreter, resized_img)
-
     interpreter.invoke()
-
     pose = common.output_tensor(interpreter, 0).copy().reshape(_NUM_KEYPOINTS, 3)
 
 
@@ -83,17 +47,71 @@ def det_pose(input):
     return np.array(img)
 
 
-import cv2
+class WebcamStream:
+    # initialization method
+    def __init__(self, stream_id=1):
+        self.stream_id = stream_id  # default is 1 for main camera
 
-# define a video capture object
-vid = cv2.VideoCapture(1)
+        # opening video capture stream
+        self.vcap = cv2.VideoCapture(self.stream_id)
+        if self.vcap.isOpened() is False:
+            print("[Exiting]: Error accessing webcam stream.")
+            exit(0)
+        fps_input_stream = int(self.vcap.get(5))  # hardware fps
+        print("FPS of input stream: {}".format(fps_input_stream))
+
+        # reading a single frame from vcap stream for initializing
+        self.grabbed, self.frame = self.vcap.read()
+        if self.grabbed is False:
+            print('[Exiting] No more frames to read')
+            exit(0)
+        # self.stopped is initialized to False
+        self.stopped = True
+        # thread instantiation
+        self.t = Thread(target=self.update, args=())
+        self.t.daemon = True  # daemon threads run in background
+
+    # method to start thread
+    def start(self):
+        self.stopped = False
+        self.t.start()
+
+    # method passed to thread to read next available frame
+    def update(self):
+        while True:
+            if self.stopped is True:
+                break
+            self.grabbed, self.frame = self.vcap.read()
+            if self.grabbed is False:
+                print('[Exiting] No more frames to read')
+                self.stopped = True
+                break
+        self.vcap.release()
+
+    # method to return latest read frame
+    def read(self):
+        return self.frame
+
+    # method to stop reading frames
+    def stop(self):
+        self.stopped = True
+
+
+# initializing and starting multi-threaded webcam input stream
+webcam_stream = WebcamStream(stream_id=1) # 0 id for main camera
+webcam_stream.start()
+# processing frames in input stream
+num_frames_processed = 0
 
 while (True):
 
     # Capture the video frame
     # by frame
-    ret, inp = vid.read()
 
+    if webcam_stream.stopped is True :
+        break
+    else:
+        inp = webcam_stream.read()
     # Display the resulting frame
     cv2.imshow('output',det_pose(inp))
 
